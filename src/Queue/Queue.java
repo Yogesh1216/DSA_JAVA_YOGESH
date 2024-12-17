e3a25a2bb61602b3835be7e3337a743b0d26eeac
Yogesh1216 <ynain1998@gmail.com>
2024-12-17 23:41:04 +0530

    DSA

---
+++ src/Queue/Queue.java
@@ -0,0 +1,10 @@
+package Queue;
+
+public class Queue {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Tree/BST.java
@@ -0,0 +1,22 @@
+package Tree;
+
+public class BST {
+
+    Node root;
+
+    public static class Node{
+        Node left;
+        int item;
+        Node right;
+
+        public Node(int data){
+            left = null;
+            item = data;
+            right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

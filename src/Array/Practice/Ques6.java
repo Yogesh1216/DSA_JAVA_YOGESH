9d10d054451fa32e4a87f6aeb9aa1833b1ec22b0
Yogesh1216 <ynain1998@gmail.com>
2025-01-04 23:09:31 +0530

    DSA

--- src/Array/Practice/Ques6.java
+++ src/Array/Practice/Ques6.java
@@ -1,6 +1,7 @@
 package Array.Practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Ques6 {
 
@@ -23,9 +24,26 @@ public class Ques6 {
         System.out.println(newArr);
     }
 
+
+    //way 2:
+    public static void removeDuplicates1(int[] arr){
+        int j=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[j]!=arr[i]){
+                j++;
+                arr[j]=arr[i];
+            }
+        }
+
+        //printing new array
+        for(int i=0;i<j+1;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {1, 1, 2, 2, 3, 3, 4};
-        removeDuplicates(arr);
+        removeDuplicates1(arr);
     }
 }
--- src/Array/Practice/Ques7.java
+++ src/Array/Practice/Ques7.java
@@ -1,6 +1,7 @@
 package Array.Practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Ques7 {
 
@@ -10,21 +11,43 @@ public class Ques7 {
             - Output: [4, 5, 1, 2, 3]
 */
 
+    //way2:
+    public static void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void rotateArrayByk(int[] arr,int k){
+        // 1)rotate full array | 2) rotate till k elements | 3) rotate from k to end of the array
+        int end = arr.length-1;
+
+        reverse(arr,0,end);
+        reverse(arr,0,k-1);
+        reverse(arr,k,end);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         int k = 2;
-
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for(int x:arr){
-            arrayList.add(x);
-        }
-
-        int count=0;
-        while(count<k){
-            arrayList.addFirst(arrayList.getLast());
-            arrayList.removeLast();
-            count++;
-        }
-        System.out.println(arrayList);
+        rotateArrayByk(arr,k);
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        for(int x:arr){
+//            arrayList.add(x);
+//        }
+//
+//        int count=0;
+//        while(count<k){
+//            arrayList.addFirst(arrayList.getLast());
+//            arrayList.removeLast();
+//            count++;
+//        }
+//        System.out.println(arrayList);
     }
 }
